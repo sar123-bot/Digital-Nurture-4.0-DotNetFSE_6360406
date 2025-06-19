0bac9955e6ed0257db0082cc259050b64e12c620
2229154 <2229154@kiit.ac.in>
2025-06-20 02:09:31 +0530

    making the final commit

---
+++ week1/Financial_Forecasting/FinancialForecast.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET.Digital_Nurture_4._0_DotNetFSE_6360406.week1.Financial_Forecasting
+{
+    public class FinancialForecast
+    {
+       
+        public static double CalculateFutureValue(double presentValue, double growthRate, int years)
+        {
+            
+            if (years == 0) return presentValue;
+
+            
+            return CalculateFutureValue(presentValue, growthRate, years - 1) * (1 + growthRate);
+        }
+    }
+
+}
---
+++ week1/Financial_Forecasting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOTNET.Digital_Nurture_4._0_DotNetFSE_6360406.week1.Financial_Forecasting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double presentValue = 1000.0;
+            double growthRate = 0.05; 
+            int years = 5;
+
+            double futureValue = FinancialForecast.CalculateFutureValue(presentValue, growthRate, years);
+            Console.WriteLine($"Future Value after {years} years at {growthRate * 100}% growth rate: {futureValue:F2}");
+        }
+    }
+}
